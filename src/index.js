1f2b745ee4bab125cb22f5a48effb1adf0e4dff1
Nicky Stevens <lnickystevens@gmail.com>
2024-07-21 13:42:13 -0400

    Added basic JavaScript for the form and added Typewriter effect

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateFairyTale(event) {
+  event.preventDefault();
 
+  new Typewriter("#story", {
+    strings: "Jack and the Beanstock",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let fairyTaleFormElement = document.querySelector("#fairy-tale-generator-form");
+fairyTaleFormElement.addEventListener("submit", generateFairyTale);
